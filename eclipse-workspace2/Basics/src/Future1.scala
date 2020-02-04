b6a107085f1143bfd85da1962fe9acb839c5c9d2
Afsana B. Roy <AfsanaRoy@Afsanas-MacBook-Pro.local>
2020-02-04 15:58:59 +0530

    Till Future

---
+++ eclipse-workspace2/Basics/src/Future1.scala
@@ -0,0 +1,14 @@
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+object Example1 extends App{
+  // used by 'time' method
+  implicit val baseTime = System.currentTimeMillis
+  // 2 - create a Future
+  val f = Future {
+     Thread.sleep(1)
+      println("This is the Future")
+  }
+  Thread.sleep(2000)
+  println("This is outside the future")
+}
